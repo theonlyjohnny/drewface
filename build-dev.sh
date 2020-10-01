3ab41dd6bb2f585735ce945b39a8b3fe16206808
ajroberts0417 <ajroberts0417@gmail.com>
2020-09-30 18:49:59 -0700

    add better build script

--- build-dev.sh
+++ build-dev.sh
@@ -2,13 +2,6 @@
 # exit on error
 set -o errexit
 
-# Install mix & elixir
-# You only need this for Ubuntu machines w/o elixir
-wget https://packages.erlang-solutions.com/erlang-solutions_2.0_all.deb && sudo dpkg -i erlang-solutions_2.0_all.deb
-sudo apt-get update
-sudo apt-get install esl-erlang
-sudo apt-get install elixir
-
 # Initial setup
 mix deps.get --only prod
 MIX_ENV=prod mix compile
